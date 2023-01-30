ce946c897d09fe98e45a31c2ce3a9f5f1a61feb1
premkumar <prem.kumar@nagarro.com>
2023-01-30 19:26:24 +0530

    mobile responsive

--- src/modules/cardWrapper.jsx
+++ src/modules/cardWrapper.jsx
@@ -3,7 +3,7 @@ import Card from "./card";
 
 const CardWrapper = ({ daily = [], tempUnit }) => {
   return (
-    <div className="p-4 grid grid-flow-col auto-cols-auto gap-2 justify-center">
+    <div className="p-4 md:px-12 grid grid-cols-[repeat(auto-fit,minmax(100px,1fr))] gap-2 justify-center">
       {daily.map((day) => (
         <Card day={day} tempUnit={tempUnit} />
       ))}
